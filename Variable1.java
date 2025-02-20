9dc1e2cc31c9570a97771793ecf71d209c5af694
aladiltanjung <aladiltanjung@gmail.com>
2025-02-20 16:17:52 +0700

    Add files via upload

---
+++ Variable1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aladil_20_02_2025;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class Variable1 {
+    public static void main(String[] arga){
+        //deklarasi a tipe data dengan nama variable
+        //result dan tipe data boolean
+        boolean result;
+        
+        //deklarasi tipe data dengan nama variable
+        //option dan tipe data char
+        char option;
+        option = 'C'; //menandai 'C' sebagai option
+        System.out.println(option);
+        
+        //deklarasi tipe data dengan nama variable
+        //grade, double tipe data dan telat di instalasi
+        //to 0.0
+        double grade = 0.0;
+        System.out.println(grade);
+    }
+}
---
+++ aritmatika1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aladil_20_02_2025;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class aritmatika1 {
+    public static void main(String[] args){
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+        System.out.println("Variable value...");
+        System.out.println(" i = " + i);
+        System.out.println(" j = " + j);
+        System.out.println(" x = " + x);
+        System.out.println(" y = " + y);
+        
+        System.out.println("Adding...");
+        System.out.println(" i + j = " + (i + j));
+        System.out.println(" x + y = " + (x + y));
+        
+        System.out.println("Subtracting...");
+        System.out.println(" i - j = " + (i - j));
+        System.out.println(" x - y = " + (x - y));
+        
+    }
+    
+}
