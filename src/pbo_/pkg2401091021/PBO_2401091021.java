c9bf5691ec4e32e7d36503dab6561eaa1e3cb054
LABP1KOMP <LABP1KOMP@LAB-P1-PC05>
2025-02-20 16:12:18 +0700

    new project

---
+++ src/pbo_/pkg2401091021/PBO_2401091021.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_.pkg2401091021;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class PBO_2401091021 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Belajar Java");
+        System.out.println("Pemograman Berorientasi Objek");
+    }
+    
+}
