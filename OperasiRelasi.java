0b87611a79c4ca99dcf9451f457f2bcac2d5792b
aladiltanjung <aladiltanjung@gmail.com>
2025-02-27 11:58:15 +0700

    Add files via upload

---
+++ OperasiRelasi.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aladiltanjung1021;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class OperasiRelasi {
+   public static void main(String[] args){
+        int i=37;
+        int j=42;
+        int k=42;
+        System.out.println("Variable values...");
+        System.out.println("i=" +i);
+        System.out.println("j=" +j);
+        System.out.println("k=" +k);
+        
+        System.out.println("Greater than...");
+        System.out.println("i > j = " +(i > j));
+        System.out.println("j > i = " +(j > i));
+        System.out.println("k > j = " +(k > j));
+        
+        System.out.println("Greater than or equal to...");
+        System.out.println(" i >= j = " + (i >= j)); 
+        System.out.println(" j >= i = " + (j >= i));
+        System.out.println(" k >= j = " + (k >= j)); 
+        
+        System.out.println("Less than...");
+        System.out.println(" i < j = " + (i < j)); 
+        System.out.println(" j < i = " + (j < i)); 
+        System.out.println(" k < j = " + (k < j)); 
+        
+        System.out.println("Less than or equal to...");
+        System.out.println(" i <= j = " + (i <= j)); 
+        System.out.println(" j <= i = " + (j <= i)); 
+        System.out.println(" k <= j = " + (k <= j));
+        
+        System.out.println("Equal to...");
+        System.out.println(" i == j = " + (i == j)); 
+        System.out.println(" k == j = " + (k == j)); 
+ 
+        System.out.println("Not equal to...");
+        System.out.println(" i != j = " + (i != j)); 
+        System.out.println(" k != j = " + (k != j));  
+    }
+    
+}
---
+++ adil260225.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aladiltanjung1021;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class adil260225 {
+    
+    public static void main(String[] args){
+        int i=10;
+        int j=3;
+        int k=0;
+        k= j++ + i;
+        System.out.println("i=" +i);
+        System.out.println("j=" +j);
+        System.out.println("k=" +k);
+    }
+}
