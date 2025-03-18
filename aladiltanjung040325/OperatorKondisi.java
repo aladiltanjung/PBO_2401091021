cf7be9a052beb693f3b9ae52d9b21dee9b76c958
aladiltanjung <aladiltanjung@gmail.com>
2025-03-18 09:39:26 +0700

    Add files via upload

---
+++ aladiltanjung040325/OperatorKondisi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aladiltanjung040325;
+
+/**
+ *
+ * @author LAB-MM
+ */
+public class OperatorKondisi {
+   public static void main( String[] args ){
+
+ String status = "";
+ int grade = 80;
+
+ status = (grade >= 60)?"Passed":"Fail";
+ System.out.println( status );
+ }  
+}
